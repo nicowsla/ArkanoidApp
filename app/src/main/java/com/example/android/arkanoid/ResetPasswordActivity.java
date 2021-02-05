2be8462898a07c652607d9450710487f1c812cd7
Nico <35233101+nicowsla@users.noreply.github.com>
2021-02-05 20:43:16 +0100

    Implementato controllo e-mail nel reset password

--- app/src/main/java/com/example/android/arkanoid/ResetPasswordActivity.java
+++ app/src/main/java/com/example/android/arkanoid/ResetPasswordActivity.java
@@ -26,10 +26,11 @@ public class ResetPasswordActivity extends NavigationMenuActivity {
 
     private FirebaseAuth mAuth;
     private EditText email;
-    private  String emailString;
+    private String emailString;
     private TextInputLayout emailLayout;
     private Animation frombottom, fromtop;
     private Button b;
+    private Boolean error = false;
 
 
     @Override
@@ -40,6 +41,9 @@ public class ResetPasswordActivity extends NavigationMenuActivity {
         dl.addView(contentView, 0);
         mAuth = FirebaseAuth.getInstance();
 
+        //nasconde il pannello delle notifiche
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
         fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
 
@@ -49,55 +53,93 @@ public class ResetPasswordActivity extends NavigationMenuActivity {
         emailLayout.startAnimation( fromtop );
         b.startAnimation( frombottom );
 
-        //nasconde il pannello delle notifiche
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        insertData();
+
+        email.setOnFocusChangeListener( new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                emailLayout.setError( null );
+                error = false;
+            }
+        } );
+
+        email.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailLayout.setError( null );
+                error = false;
+            }
+        } );
+    }
+
+    public void insertData(){
+        emailString = email.getText().toString();
+    }
+
+
+    public void verifyCredentials(){
+        if(emailString == null || emailString.length()<1){
+            emailLayout.setError( getString(R.string.signin_email_error) );
+            error = true;
+
+        }else if(!isValidEmailAddress(emailString)){
+            emailLayout.setError( getString(R.string.signin_email_error) );
+            error = true;
+        }
     }
 
     public  void resetPassword(View view){
         insertData();
-        mAuth.fetchSignInMethodsForEmail(emailString)
-                .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+        verifyCredentials();
 
-                        boolean emailNotExists = task.getResult().getSignInMethods().isEmpty();
+        if(!error) {
+            mAuth.fetchSignInMethodsForEmail(emailString)
+                    .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
 
-                        if (emailNotExists) {
-                            AlertDialog alertDialog = new AlertDialog.Builder(ResetPasswordActivity.this).create();
-                            alertDialog.setTitle(getString(R.string.error));
-                            alertDialog.setMessage(getString(R.string.email_nor_registered));
-                            alertDialog.setCanceledOnTouchOutside(false);
-                            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                                    new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            dialog.dismiss();
-                                            startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
-                                        }
-                                    });
-                            alertDialog.show();
-                        } else {
-                            mAuth.sendPasswordResetEmail(emailString);
-                            AlertDialog alertDialog = new AlertDialog.Builder(ResetPasswordActivity.this).create();
-                            alertDialog.setTitle(getString(R.string.reset_psw));
-                            alertDialog.setMessage(getString(R.string.check_email));
-                            alertDialog.setCanceledOnTouchOutside(false);
-                            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                                    new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            dialog.dismiss();
-                                            startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
-                                        }
-                                    });
-                            alertDialog.show();
+                            boolean emailNotExists = task.getResult().getSignInMethods().isEmpty();
+
+                            if (emailNotExists && !error) {
+                                AlertDialog alertDialog = new AlertDialog.Builder(ResetPasswordActivity.this).create();
+                                alertDialog.setTitle(getString(R.string.error));
+                                alertDialog.setMessage(getString(R.string.email_nor_registered));
+                                alertDialog.setCanceledOnTouchOutside(false);
+                                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getText(R.string.commands_not_confirm),
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.dismiss();
+                                                startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+                                            }
+                                        });
+                                alertDialog.show();
+                            } else {
+                                mAuth.sendPasswordResetEmail(emailString);
+                                AlertDialog alertDialog = new AlertDialog.Builder(ResetPasswordActivity.this).create();
+                                alertDialog.setTitle(getString(R.string.reset_psw));
+                                alertDialog.setMessage(getString(R.string.check_email));
+                                alertDialog.setCanceledOnTouchOutside(false);
+                                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getText(R.string.commands_not_confirm),
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                dialog.dismiss();
+                                                startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+                                            }
+                                        });
+                                alertDialog.show();
+                            }
+
                         }
-
-                    }
-                });
+                    });
+        }
 
     }
 
-    public  void insertData(){
-        emailString = email.getText().toString();
+    public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
     }
 
 }
--- app/src/main/java/com/example/android/arkanoid/SignInActivity.java
+++ app/src/main/java/com/example/android/arkanoid/SignInActivity.java
@@ -165,7 +165,6 @@ public class SignInActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
@@ -238,9 +237,8 @@ public class SignInActivity extends AppCompatActivity {
                             DatabaseReference myRef = database.getReference( "utenti" ).child( user.getUid() );
                             myRef.setValue( new User(user.getUid(),username, email, 0, 1000000000) );
 
-
-
                             Toast.makeText( getApplicationContext(), R.string.signin_check_mail, Toast.LENGTH_LONG ).show();
+
                             AlertDialog alertDialog = new AlertDialog.Builder( SignInActivity.this ).create();
                             alertDialog.setTitle( R.string.attention);
                             alertDialog.setMessage(getString(R.string.signin_check_mail));
@@ -266,10 +264,7 @@ public class SignInActivity extends AppCompatActivity {
                                         }
                                     } );
                             alertDialog.show();
-
                         }
-
-                        // ...
                     }
                 });
         }
